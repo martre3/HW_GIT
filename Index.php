8eae34db466a0e9aab61e4c58e37729e9946b3c3
Martynas Treinys <martynas7928@gmail.com>
2017-10-08 17:03:38 +0300

    A list updated and status updated

--- Index.php
+++ Index.php
@@ -5,8 +5,9 @@
  * Date: 17.10.8
  * Time: 16.49
  */
-echo "<h1>Homework status: in progress</h1>";
+echo "<h1>Homework status: Completed</h1>";
 echo "<ul>";
 echo "<li> Status updated</li>";
 echo "<li> Branch created</li>";
+echo "<li> Branches merged and conflicts resolver </li>";
 echo "</ul>";
